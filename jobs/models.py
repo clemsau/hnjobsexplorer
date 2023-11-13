8ea9f085c08c853c2ba2f0958b21817f800e2923
Clement Sauvage <clement.sauvage@adevinta.com>
2023-11-13 21:48:57 +0100

    Add Thread & Job models and make migrations

---
+++ jobs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Thread(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=100)
+    date = models.CharField(max_length=100)
+
+
+class Job(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, primary_key=True)
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    body = models.TextField()
