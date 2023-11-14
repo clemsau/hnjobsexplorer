f41fc16d5da168f24eb349a96eb89132499b2918
Clement Sauvage <clement.sauvage@adevinta.com>
2023-11-14 23:30:41 +0100

    Add Month select in form

--- jobs/views.py
+++ jobs/views.py
@@ -1,15 +1,25 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import View
+from django.views.generic.base import ContextMixin
 
 from .models import Thread, Job
 
 
-class IndexView(TemplateView):
+class IndexView(View, ContextMixin):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-
+        context = super().get_context_data(**kwargs)
         context["threads"] = Thread.objects.all()
-        context["jobs"] = Job.objects.all()[0:30]
-
+        context["jobs"] = Job.objects.filter(thread=kwargs["current_thread_id"])
         return context
+
+    def get(self, request, *args, **kwargs):
+        kwargs["current_thread_id"] = Thread.objects.last().id
+        return render(request, self.template_name, self.get_context_data(**kwargs))
+
+    def post(self, request, *args, **kwargs):
+        kwargs["current_thread_id"] = request.POST["current_thread_id"]
+        context = self.get_context_data(**kwargs)
+        context["jobs"] = context["jobs"].filter(body__icontains=request.POST["search"])
+        return render(request, "components/listing.html", context)
