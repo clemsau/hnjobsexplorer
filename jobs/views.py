8259579aec651554661663eda00f15afc8b76773
Clement Sauvage <clement.sauvage@adevinta.com>
2023-11-14 21:49:23 +0100

    Display threads months in dropdown

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from .models import Thread
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context["threads"] = Thread.objects.all()
+
+        return context
