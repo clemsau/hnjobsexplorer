a4cc0a161396acbe16f82b422ba90c0f0767941d
Clement Sauvage <clement.sauvage@adevinta.com>
2023-11-16 16:24:46 +0100

    Fix jobs fetchin bug

--- jobs/management/commands/fetch_new_jobs.py
+++ jobs/management/commands/fetch_new_jobs.py
@@ -11,7 +11,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            new_kids = list()
             latest_threads = Thread.objects.all().order_by('timestamp')[:2]
             for latest_thread in latest_threads:
                 response = requests.get(HN_ITEM_URL.format(latest_thread.id))
@@ -20,24 +19,21 @@ class Command(BaseCommand):
 
                 kids = set(response.json()['kids'])
                 saved_kids = set(map(int, [job.id for job in latest_thread.job_set.all()]))
-                new_kids += list(kids - saved_kids)
-                print(latest_thread)
 
-            for job_id in new_kids:
-                print(new_kids)
-                job, created = Job.objects.get_or_create(id=job_id, thread=latest_thread)
-                if not created:
-                    continue
+                for job_id in (kids - saved_kids):
+                    job, created = Job.objects.get_or_create(id=job_id, thread=latest_thread)
+                    if not created:
+                        continue
 
-                response = requests.get(HN_ITEM_URL.format(job_id))
-                if response.status_code != 200:
-                    raise CommandError('Unable to fetch latest jobs')
+                    response = requests.get(HN_ITEM_URL.format(job_id))
+                    if response.status_code != 200:
+                        raise CommandError('Unable to fetch latest jobs')
 
-                job.body = response.json().get('text', '')
-                if not job.body:
-                    job.deactivated = True
+                    job.body = response.json().get('text', '')
+                    if not job.body:
+                        job.deactivated = True
 
-                job.save()
+                    job.save()
 
         except Exception as e:
             raise CommandError('Unable to fetch latest jobs: {}'.format(e))
